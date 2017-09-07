3b7ffa697426b56eeb05447ecc6589301386702a
Askar Bazayev <zurbagan0409@gmail.com>
2017-09-07 15:24:43 +0600

    app.js was broken by git, models/dbs.js

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
 var express = require('express');
 var Sequelize = require('sequelize');
-<<<<<<< HEAD
-
-// var connect = require('connect')
-// for express, just call it with 'require('express-session').Store' 
-=======
 var db = require('./models/dbs.js');
 var database = new db();
 database.con.sync();
 
 // var connect = require('connect')
     // for express, just call it with 'require('express-session').Store' 
->>>>>>> ee0c3e1747b651db2b8fe13d3db6e5ce95ea3741
 // var SequelizeStore = require('connect-session-sequelize')(connect.session.Store);
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
@@ -20,33 +14,13 @@ var session = require('express-session');
 var SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // create database, ensure mysql ('sqlite3') in your package.json 
-<<<<<<< HEAD
-var connection = new Sequelize(
-    'warehouse',
-    'root',
-    'Mandriva2012', {
-        'dialect': 'mysql' //, 
-            //   "storage": "./session.sqlite"
-    });
-require('events').EventEmitter.defaultMaxListeners = 20;
-=======
 
 
->>>>>>> ee0c3e1747b651db2b8fe13d3db6e5ce95ea3741
 // configure express 
 var app = express();
 app.use(cookieParser());
 
 app.use(session({
-<<<<<<< HEAD
-    secret: 'keyboard Alasha2017',
-    resave: true,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-            db: connection
-        }) //,
-        //  proxy: true // if you do SSL outside of node. 
-=======
   secret: 'keyboard Alasha2017',
     resave: true,
     saveUninitialized: true,
@@ -54,7 +28,6 @@ app.use(session({
     db: database.con
   }) //,
 //  proxy: true // if you do SSL outside of node. 
->>>>>>> ee0c3e1747b651db2b8fe13d3db6e5ce95ea3741
 }));
 // continue as normal  
 // initalize sequelize with session store 
@@ -64,37 +37,20 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-<<<<<<< HEAD
-var bcrypt = require('bcrypt');
+// var bcrypt = require('bcryptjs');
 var index = require('./routes/index');
-var users = require('./routes/users');
-// var birds = require('./routes/birds');
-=======
- var bcrypt = require('bcrypt');
-var index = require('./routes/index');
->>>>>>> ee0c3e1747b651db2b8fe13d3db6e5ce95ea3741
 var register = require('./routes/register');
 var profile = require('./routes/profile');
 var users_list = require('./routes/users_list');
 var user_details = require('./routes/user_details');
-var orders_list = require('./routes/orders_list');
-var order_details = require('./routes/order_details');
-var create_order = require('./routes/create_order');
 var login = require('./routes/login');
 var logout = require('./routes/logout');
-<<<<<<< HEAD
-=======
 //var main = require('./main');
 const ctrls = require('./controllers')
->>>>>>> ee0c3e1747b651db2b8fe13d3db6e5ce95ea3741
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-<<<<<<< HEAD
-
-=======
->>>>>>> ee0c3e1747b651db2b8fe13d3db6e5ce95ea3741
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -104,65 +60,30 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'files')));
 app.use(express.static(path.join(__dirname, 'upload')));
-<<<<<<< HEAD
-
-app.use('/', index);
-// app.use('/users', users);
-// app.use('/birds', birds);
-=======
 app.use(express.static(__dirname + '/dest'));
 app.use(express.static(__dirname + '/img'));
 app.use(express.static(__dirname + '/dest/images'));
 
 
 app.use('/', index);
->>>>>>> ee0c3e1747b651db2b8fe13d3db6e5ce95ea3741
 app.use('/register', register);
 app.use('/profile', profile);
 app.use('/users_list', users_list);
 app.use('/user_details', user_details);
-app.use('/order_details', order_details);
-app.use('/orders_list', orders_list);
-app.use('/create_order', create_order);
 app.use('/login', login);
 app.use('/logout', logout);
-<<<<<<< HEAD
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-=======
 app.get('/build', ctrls.categories);
-app.post('/build', ctrls.tabb_post);
+app.get('/category', ctrls.products);
 //app.use('/main', main); // de Khan
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
->>>>>>> ee0c3e1747b651db2b8fe13d3db6e5ce95ea3741
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-<<<<<<< HEAD
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
-
-app.listen(3000, function() {
-    console.log('Example app listening on port 3000!');
-});
-
-module.exports = app;
-=======
   // set locals, only providing error in development
   res.locals.message = err.message;
   //res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -181,4 +102,3 @@ app.listen(3000, function () {
  });
 
 module.exports = app;
->>>>>>> ee0c3e1747b651db2b8fe13d3db6e5ce95ea3741
--- models/dbs.js
+++ models/dbs.js
@@ -1,152 +1,21 @@
-<<<<<<< HEAD
-=======
-const config = require('../config/db')
->>>>>>> ee0c3e1747b651db2b8fe13d3db6e5ce95ea3741
+const config = require('../config/db_wh')
 var Sequelize = require('sequelize');
 var mysql = require('mysql2');
 var bcrypt = require('bcrypt');
 var session = require('express-session');
 var async = require('async');
-<<<<<<< HEAD
-var connection = new Sequelize('warehouse', 'root', 'Mandriva2012', {
-    dialect: 'mysql'
-});
-var SequelizeStore = require('connect-session-sequelize')(session.Store);
-class DBS { // database with Sequelize
-    constructor(host, dbusername, dbpassword, dbname) {
-            this._host = host;
-            this._dbusername = dbusername;
-            this._dbpassword = dbpassword;
-            this._dbname = dbname;
-            var con = new Sequelize(this._dbname, this._dbusername, this._dbpassword, { dialect: 'mysql' });
-            this.con = con;
-            this.Clients = connection.define('clients', {
-                id: {
-                    type: Sequelize.INTEGER,
-                    primaryKey: true,
-                    autoIncrement: false
-                },
-                name: Sequelize.STRING,
-                title: Sequelize.STRING,
-                description: Sequelize.STRING,
-                address: Sequelize.STRING,
-                bin_iin: Sequelize.STRING,
-                bank_data: Sequelize.STRING,
-                status: Sequelize.INTEGER,
-                rating: Sequelize.INTEGER
-            }, { timestamps: false });
-            this.Users = connection.define('users', {
-                id: {
-                    type: Sequelize.INTEGER,
-                    primaryKey: true,
-                    autoIncrement: true
-                },
-                lastname: Sequelize.STRING,
-                firstname: Sequelize.STRING,
-                email: Sequelize.STRING,
-                password: Sequelize.STRING,
-                client_id: Sequelize.INTEGER
-            }, { timestamps: false });
-            this.Orders = connection.define('orders', {
-                id: {
-                    type: Sequelize.INTEGER,
-                    primaryKey: true,
-                    autoIncrement: true
-                },
-                document_no: Sequelize.STRING,
-                document_date: Sequelize.DATE,
-                order_status_id: Sequelize.INTEGER,
-                order_types_id: Sequelize.INTEGER,
-                contragent_id: Sequelize.INTEGER,
-                client_id: Sequelize.INTEGER,
-                user_id: Sequelize.INTEGER
-
-            }, { timestamps: false });
-            this.Order_types = connection.define('order_types', {
-                id: {
-                    type: Sequelize.INTEGER,
-                    primaryKey: true,
-                    autoIncrement: true
-                },
-                name: Sequelize.STRING
-
-            }, { timestamps: false });
-            // this.Clients.hasMany(this.Users); // Will add client_id to user
-            this.Session = connection.define('Session', {
-                sid: {
-                    type: Sequelize.STRING,
-                    primaryKey: true
-                },
-                userId: Sequelize.STRING,
-                expires: Sequelize.DATE,
-                data: Sequelize.STRING(50000)
-            }, { timestamps: false });
-            var extendDefaultFields = function(defaults, session) {
-                    return {
-                        data: defaults.data,
-                        expires: defaults.expires,
-                        userId: session.userId
-                    };
-                }
-                //var store = new SessionStore({
-            this.store = new SequelizeStore({
-                db: con,
-                table: 'Session',
-                extendDefaultFields: extendDefaultFields
-            });
-
-        } // constructor
-
-    createUser(req, res) {
-        var firstname = req.body.firstname;
-        var lastname = req.body.lastname;
-        var email = req.body.email;
-        var password = req.body.password;
-        var city = req.body.city;
-        var name = req.body.name;
-        //        let hash = bcrypt.hashSync(password, 10);
-        // Generate a salt
-        var salt = bcrypt.genSaltSync(10);
-        // Hash the password with the salt
-        var hash = bcrypt.hashSync(req.body.password, salt);
-        let sql = "INSERT INTO `users`(`id`, `firstname`, `lastname`, `email`, `password`) VALUES ('','" + firstname + "','" + lastname + "','" + email + "','" + hash + "')";
-        this.con.query(sql, (err, result) => {
-            if (err) {
-                console.log(err);
-                throw err;
-            }
-            res.redirect('/login');
-        });
-    }
-
-    checkUser(req, res) {
-        var email = req.body.email;
-        var password = req.body.password;
-        let sql = "SELECT * FROM `users` WHERE email='" + email + "'";
-        return this.con.query(sql, (err, result, fields) => {
-            if (err) throw err;
-            if (result.length === 1) {
-                if (bcrypt.compareSync(password, result[0].password)) {
-                    req.session.user_id = result[0].id;
-                    req.session.email = result[0].email;
-                    res.redirect('/profile');
-                } else {
-                    res.redirect('/login?error=1');
-                }
-            } else {
-=======
 const connection = new Sequelize(`${config.engine}://${config.username}:${config.password}@${config.host}/${config.database}`)
- 
+
 var SequelizeStore = require('connect-session-sequelize')(session.Store);
- class DBS {		// database with Sequelize
-    constructor (){
-        this._host = '{config.host}';
-        this._dbusername = '${config.username}';
-        this._dbpassword = '${config.password}';
-        this._dbname = '${config.database}';
-        this._engine = '${config.engine}';
-        this.con = connection;
-	this.Clients = connection.define('clients', {
+class DBS {		// database with Sequelize
+   constructor (){
+       this._host = '{config.host}';
+       this._dbusername = '${config.username}';
+       this._dbpassword = '${config.password}';
+       this._dbname = '${config.database}';
+       this._engine = '${config.engine}';
+       this.con = connection;
+   this.Clients = connection.define('clients', {
 	id: {
              type: Sequelize.INTEGER,
              primaryKey: true,
@@ -161,54 +30,52 @@ var SequelizeStore = require('connect-session-sequelize')(session.Store);
  	status:Sequelize.INTEGER,
 	rating:Sequelize.INTEGER
 	},{timestamps: false});
-	this.Users = this.con.define('users', {
+	this.Users = connection.define('users', {
 	id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
+    	     type: Sequelize.INTEGER,
+             primaryKey: true,
+             autoIncrement: true
   	},
 	lastname:Sequelize.STRING,
 	firstname: Sequelize.STRING,
 	email: Sequelize.STRING,
-	password: Sequelize.STRING
- 
-	},{timestamps: false});
-
-this.Orders = connection.define('orders', {
-    id: {
-             type: Sequelize.INTEGER,
-             primaryKey: true,
-             autoIncrement: true
-      },
-    document_no:Sequelize.STRING,
-    document_date: Sequelize.DATE,
-    order_status_id: Sequelize.INTEGER,
-    order_types_id: Sequelize.INTEGER,
-    contragent_id: Sequelize.INTEGER,
-    client_id: Sequelize.INTEGER,
-    user_id: Sequelize.INTEGER
-
-},{timestamps: false});
-this.Order_types = connection.define('order_types', {
-    id: {
-             type: Sequelize.INTEGER,
-             primaryKey: true,
-             autoIncrement: true
-      },
-    name:Sequelize.STRING
+	password: Sequelize.STRING,
+ 	client_id:Sequelize.INTEGER
+    },{timestamps: false});
+	this.Orders = connection.define('orders', {
+        id: {
+                 type: Sequelize.INTEGER,
+                 primaryKey: true,
+                 autoIncrement: true
+          },
+        document_no:Sequelize.STRING,
+        document_date: Sequelize.DATE,
+        order_status_id: Sequelize.INTEGER,
+        order_types_id: Sequelize.INTEGER,
+        contragent_id: Sequelize.INTEGER,
+        client_id: Sequelize.INTEGER,
+        user_id: Sequelize.INTEGER
     
+    },{timestamps: false});
+	this.Order_types = connection.define('order_types', {
+        id: {
+                 type: Sequelize.INTEGER,
+                 primaryKey: true,
+                 autoIncrement: true
+          },
+        name:Sequelize.STRING
+        
+    },{timestamps: false});
+        // this.Clients.hasMany(this.Users); // Will add client_id to user
+	this.Session = connection.define('Session', {
+  sid: {
+    type: Sequelize.STRING,
+    primaryKey: true
+  },
+  userId: Sequelize.STRING,
+  expires: Sequelize.DATE,
+  data: Sequelize.STRING(50000)
 },{timestamps: false});
-    // this.Clients.hasMany(this.Users); // Will add client_id to user
-this.Session = connection.define('Session', {
-sid: {
-type: Sequelize.STRING,
-primaryKey: true
-},
-userId: Sequelize.STRING,
-expires: Sequelize.DATE,
-data: Sequelize.STRING(20000)
-},{timestamps: false});
-
 var extendDefaultFields= function(defaults, session){
   return {
     data: defaults.data,
@@ -218,13 +85,13 @@ var extendDefaultFields= function(defaults, session){
 }
  //var store = new SessionStore({
   this.store = new SequelizeStore({
-  db: this.con,
+  db: connection,
   table: 'Session',
   extendDefaultFields: extendDefaultFields
 });
 
    } // constructor
-/* 
+ 
     createUser(req, res){
         var firstname = req.body.firstname;
         var lastname = req.body.lastname;
@@ -262,21 +129,10 @@ var hash = bcrypt.hashSync(req.body.password, salt);
                     res.redirect('/login?error=1');
                 }
             }else{
->>>>>>> ee0c3e1747b651db2b8fe13d3db6e5ce95ea3741
                 res.redirect('/login?error=1');
             }
         })
     }
-<<<<<<< HEAD
-    deleteUser(req, res) {
-        var email = req.body.email;
-        var password = req.body.password;
-        let sql = "DELETE FROM `users` WHERE email='" + email + "'";
-        return this.con.query(sql, (err, result, fields) => {
-            if (err) {
-                throw err;
-            } else {
-=======
     deleteUser(req, res){
         var email = req.body.email;
         var password = req.body.password;
@@ -285,14 +141,9 @@ var hash = bcrypt.hashSync(req.body.password, salt);
             if(err){
             	 throw err;
             }else{
->>>>>>> ee0c3e1747b651db2b8fe13d3db6e5ce95ea3741
                 res.redirect('/users_list');
             }
         })
     }
-<<<<<<< HEAD
-=======
-*/    
->>>>>>> ee0c3e1747b651db2b8fe13d3db6e5ce95ea3741
 }
 module.exports = DBS;
