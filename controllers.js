af82051c247b6155595c5ae1ca9b25f0d956f054
Kang De Khan <kan_dehan@mail.ru>
2017-09-07 16:25:08 +0600

    DeKhan

--- controllers.js
+++ controllers.js
@@ -1,106 +1,97 @@
 const models= require('./models')
-<<<<<<< HEAD
 
 exports = module.exports = {}
 
-
-
-=======
-exports = module.exports = {}
-var routes = {};
-var tabb = 5;
-var session = require('express-session');
- session.tabb=5;
-
-exports.tabb_get = function(req, res) {
-   
-    if(req.session.tabb){  
-          
-        res.redirect('./build');
-    }else{
-        res.render('tabb');}
-};
-exports.tabb_post = function(req, res) {
-    
-     if(req.session.tabb){
-         res.redirect  
-           ('./build');
-     }else{};
-   
- };
-
-
-    
->>>>>>> ee0c3e1747b651db2b8fe13d3db6e5ce95ea3741
 exports.categories = function(req, res) {
-    models.Category.findAll().then(function(category) { 
-        models.Products.findAll().then(function(products) {
-            models.Subcat.findAll().then(function(subcat) {
-<<<<<<< HEAD
-                
-                var tabb = req.query.cat
-               
-=======
-                var tabb = req.query.cat
->>>>>>> ee0c3e1747b651db2b8fe13d3db6e5ce95ea3741
-   	    res.render('./build', { 'category': category,'products': products, 'subcat': subcat, 'tabb':tabb})          
+
+    var tabb = req.query.cat   
+    models.Subcat.findAll().then(function(subcat) {
+
+        models.Category.findAll().then(function(category) { 
+
+            models.Products.findAll().then(function(products) {
+           
+                res.render('./build', { 'category': category,'products': products, 'subcat': subcat,'tabb':tabb})          
+            });
         });
     });
-    });
-   }
-<<<<<<< HEAD
-   
-   exports.products = function(req, res) {
+}
+
+exports.products = function(req, res) {
        
     models.Category.findAll().then(function(category) { 
+
         var sabb = req.query.prod
         var priceup =req.body.priceup
         var pricedown=req.body.pricedown  
-         console.log(priceup)
-         console.log(pricedown)
-        if((pricedown!=null)&&(priceup!=null)){
-        models.Products.findAll({
-            where:{
-                price:{
-                    between:[pricedown,priceup]
-                }
+        var filtr=req.query.filtr        
+        if(filtr!=null){
 
+            if((pricedown!=null)&&(priceup!=null)){
+
+                models.Products.findAll({
+
+                    where:{
+
+                        price:{
+
+                            between:[pricedown,priceup]
+                        },
+                        category_id:filtr
+                    }
+                }).then(function(products) {
+
+                    models.Subcat.findAll().then(function(subcat) { 
+
+                        res.render('./category', { 'category': category,'products': products, 'subcat': subcat, 'sabb':sabb})          
+                   });
+                });
+            }else{
+
+                models.Products.findAll({
+
+                    where:{
+
+                        category_id:filtr       
+                    }
+                }).then(function(products) {
+
+                    models.Subcat.findAll().then(function(subcat) { 
+
+                          res.render('./category', { 'category': category,'products': products, 'subcat': subcat, 'sabb':sabb})          
+                    });
+                });
+        }}else{
+
+            if((pricedown!=null)&&(priceup!=null)){
+
+                models.Products.findAll({
+
+                    where:{
+
+                        price:{
+
+                            between:[pricedown,priceup]
+                        }
+                    }
+                }).then(function(products) {
+
+                    models.Subcat.findAll().then(function(subcat) {
+
+                        res.render('./category', { 'category': category,'products': products, 'subcat': subcat, 'sabb':sabb})          
+                    });
+                });
+            }else{
+
+                models.Products.findAll().then(function(products) {
+
+                    models.Subcat.findAll().then(function(subcat) {  
+
+                        res.render('./category', { 'category': category,'products': products, 'subcat': subcat, 'sabb':sabb})          
+           
+                    });
+                });
             }
-        }         
-        ).then(function(products) {
-            models.Subcat.findAll().then(function(subcat) {             
-               
-   	    res.render('./category', { 'category': category,'products': products, 'subcat': subcat, 'sabb':sabb})          
-       console.log(priceup, pricedown)
+        }
     });
-    });
-}else{
-    models.Products.findAll().then(function(products) {
-        models.Subcat.findAll().then(function(subcat) {           
-                  res.render('./category', { 'category': category,'products': products, 'subcat': subcat, 'sabb':sabb})          
-   
-});
-});
-
-
 }
-
-
-
-    });
-   }
-
-    
-=======
-
->>>>>>> ee0c3e1747b651db2b8fe13d3db6e5ce95ea3741
-
-
-    // .catch(function(error){
-    //     res.status(500).send({
-    //         error: 'Oooops'
-    //     })
-    // })
-
-
-
--- models.js
+++ models.js
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
 const config = require('./config/db')
 
 const Sequelize = require('sequelize')
 const sqlize = new Sequelize(`${config.engine}://${config.username}:${config.password}@${config.host}/${config.database}`)
 
 var exports = module.exports ={}
-exports.Products = sqlize.define('products' ,{
+exports.Products = sqlize.define('aproducts' ,{
     name: Sequelize.STRING,
     width: Sequelize.FLOAT,
     height: Sequelize.FLOAT,
@@ -29,37 +28,3 @@ exports.Subcat = sqlize.define('subcat' ,{
    
 },{timestamps: false})
 
-// exports.aaaa=sqlize.query('SELECT * FROM subcats WHERE category_id = 9 ',
-// { replacements: { status: 'active' }, type: sqlize.QueryTypes.SELECT }
-// )
-=======
-const config = require('./config/db')
-const ejs = require
-const Sequelize = require('sequelize')
-const sqlize = new Sequelize(`${config.engine}://${config.username}:${config.password}@${config.host}/${config.database}`)
-
-var exports = module.exports ={}
-exports.Products = sqlize.define('products' ,{
-    name: Sequelize.STRING,
-    width: Sequelize.FLOAT,
-    height: Sequelize.FLOAT,
-    lenght: Sequelize.FLOAT,
-    volume: Sequelize.FLOAT,
-    weight: Sequelize.FLOAT,
-    qty: Sequelize.INTEGER,
-    price: Sequelize.INTEGER,
-    category_id: Sequelize.INTEGER,
-},{timestamps: false})
-
-exports.Category = sqlize.define('category' ,{
-    name: Sequelize.STRING,
-   
-},{timestamps: false})
-
-
-exports.Subcat = sqlize.define('subcat' ,{
-    category_id: Sequelize.INTEGER,
-    subcat_id: Sequelize.INTEGER,
-   
-},{timestamps: false})
->>>>>>> ee0c3e1747b651db2b8fe13d3db6e5ce95ea3741
